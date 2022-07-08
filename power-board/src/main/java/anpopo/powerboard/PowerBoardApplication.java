8558f51b359bfe8e37c7857ce522d8193c6ac6fe
ansehyeong <dkstpgud@gmail.com>
2022-07-08 09:28:20 +0900

    initial commit

---
+++ power-board/src/main/java/anpopo/powerboard/PowerBoardApplication.java
@@ -0,0 +1,13 @@
+package anpopo.powerboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PowerBoardApplication.class, args);
+    }
+
+}
---
+++ power-board/src/test/java/anpopo/powerboard/PowerBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package anpopo.powerboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
