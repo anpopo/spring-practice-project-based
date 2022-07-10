841375dbc8a4383fb4f2d96ef76d97989ce564c4
ansehyeong <dkstpgud@gmail.com>
2022-07-11 08:42:49 +0900

    #10 - domain [feat] 게시판 erd 설계 자바 코드로 변환
    - erd 로 모델링 된 테이블을 자바 객체 코드로 변환
    - 멤버에 관한 erd 모델은 추후 추가 예정

---
+++ power-board/src/main/java/anpopo/powerboard/domain/Article.java
@@ -0,0 +1,15 @@
+package anpopo.powerboard.domain;
+
+import java.time.LocalDateTime;
+
+public class Article {
+    private Long articleId;
+    private String title;
+    private String content;
+    private String hashtag;
+
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime modifiedAt;
+    private String modifiedBy;
+}
