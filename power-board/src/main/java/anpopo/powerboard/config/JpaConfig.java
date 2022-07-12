c84fb9a43c7d3114141be3109ef8cf43f0d2c278
ansehyeong <dkstpgud@gmail.com>
2022-07-12 09:23:25 +0900

    #12-database-setting [feat] java object -> entity 로 코드 추가 및 변경
    - article entity
    - jpa audit 을 위한 config 구성

---
+++ power-board/src/main/java/anpopo/powerboard/config/JpaConfig.java
@@ -0,0 +1,20 @@
+package anpopo.powerboard.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        // TODO: 스프링 시큐리티로 인증 기능을 추가하는 경우 수정 해야할 코드
+        return () -> Optional.of("anpopo");
+    }
+
+}
--- power-board/src/main/java/anpopo/powerboard/domain/Article.java
+++ power-board/src/main/java/anpopo/powerboard/domain/Article.java
@@ -1,15 +1,82 @@
 package anpopo.powerboard.domain;
 
-import java.time.LocalDateTime;
+import anpopo.powerboard.config.JpaConfig;
+import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy"),
+})
+@Entity
 public class Article {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long articleId;
+
+    @Setter
+    @Column(nullable = false)
     private String title;
+
+    @Setter
+    @Column(nullable = false, length = 10000)
     private String content;
+
+    @Setter
+    @Column
     private String hashtag;
 
+    @CreatedDate
+    @Column(nullable = false)
     private LocalDateTime createdAt;
+
+    @CreatedBy
+    @Column(nullable = false, length = 100)
     private String createdBy;
+
+    @LastModifiedDate
+    @Column(nullable = false)
     private LocalDateTime modifiedAt;
+
+    @LastModifiedBy
+    @Column(nullable = false, length = 100)
     private String modifiedBy;
+
+    private Article(String title, String content, String hashtag) {
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+    }
+
+    public static Article of(String title, String content, String hashtag) {
+        return new Article(title, content, hashtag);
+    }
+
+    /**
+     * - 영속화 되지 않은 엔티티는 전부 다른 값으로 본다.
+     * - 자바 14부터 도입된 패턴 변수 -> article 키워드 사용
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Article article)) return false;
+        return articleId != null && articleId.equals(article.articleId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(articleId);
+    }
 }
