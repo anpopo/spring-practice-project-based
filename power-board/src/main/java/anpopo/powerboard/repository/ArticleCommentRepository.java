ce6ad1dc68695ff3120436cf6c132e9320014b91
ansehyeong <dkstpgud@gmail.com>
2022-07-14 08:18:00 +0900

    #5-api [feat] data rest 를 이용해 기본 crud api 생성, rest 로 생성된 api 통합 테스트 작성
    - data rest 를 사용해서 @RepositoryRestResource 어노테이션이 있는 레포지토리 기반으로 api 를 만들어 준다.
    - 해당 api 를 테스트 코드를 이용해 테스트
      - slice test (@WebMvcTest) 를 사용하지 않았는데 컨트롤러를 제외한 빈을 등록시키지 않기 때문에 repository에 있는 어노테이션을 인식하지 못하고 api 를 찾지 못한다.
      - 그래서 통합 테스트로 env 를 MOCK 으로 테스트 진행
      - MockMvc 사용을 위해선 @AutoConfigureMockMvc어노테이션 필요

--- power-board/src/main/java/anpopo/powerboard/repository/ArticleCommentRepository.java
+++ power-board/src/main/java/anpopo/powerboard/repository/ArticleCommentRepository.java
@@ -2,6 +2,8 @@ package anpopo.powerboard.repository;
 
 import anpopo.powerboard.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
--- power-board/src/main/java/anpopo/powerboard/repository/ArticleRepository.java
+++ power-board/src/main/java/anpopo/powerboard/repository/ArticleRepository.java
@@ -2,6 +2,8 @@ package anpopo.powerboard.repository;
 
 import anpopo.powerboard.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
