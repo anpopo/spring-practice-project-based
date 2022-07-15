8f3e685f0e655b85c5536aa53480765bfd46d97b
ansehyeong <dkstpgud@gmail.com>
2022-07-15 09:39:24 +0900

    #15-view [feat] 게시판 페이지 , 게시글 페이지 생성 - view controller 생성 - 게시판, 게시글 페이지 구현 - header, footer thymeleaf decoupled logic 사용 - 테스트 통과

--- power-board/src/main/java/anpopo/powerboard/controller/ArticleController.java
+++ power-board/src/main/java/anpopo/powerboard/controller/ArticleController.java
@@ -1,9 +1,39 @@
 package anpopo.powerboard.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collections;
 
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
+
+    @GetMapping
+    public String articles(Model model) {
+        model.addAttribute("articles", Collections.emptyList());
+        return "articles/index";
+    }
+
+    @GetMapping("/{articleId}")
+    public String articleDetail(@PathVariable("articleId") Long articleId, Model model) {
+        model.addAttribute("article", new Object());
+        model.addAttribute("articleComments", Collections.emptyList());
+        return "articles/detail";
+    }
+
+    @GetMapping("/search")
+    public String articlesSearch(Model model) {
+        model.addAttribute("article", new Object());
+        model.addAttribute("articleComments", Collections.emptyList());
+        return "articles/search";
+    }
+
+    @GetMapping("/search-hashtag")
+    public String articlesSearchHashtag(Model model) {
+        return "articles/search-hashtag";
+    }
 }
--- power-board/src/test/java/anpopo/powerboard/controller/ArticleControllerTest.java
+++ power-board/src/test/java/anpopo/powerboard/controller/ArticleControllerTest.java
@@ -10,7 +10,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@Disabled("구현중 뷰 테스트 먼저 정의 해둠")
 @DisplayName("Article view controller 테스트")
 @WebMvcTest(ArticleController.class)
 class ArticleControllerTest {
@@ -18,14 +17,13 @@ class ArticleControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-
     @DisplayName("[view] [GET] 게시글 리스트 게시판 페이지 - 정상 호출")
     @Test
     void articleList() throws Exception {
         // when & then
         mockMvc.perform(MockMvcRequestBuilders.get("/articles"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.TEXT_HTML))
+                .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(MockMvcResultMatchers.view().name("articles/index"))
                 .andExpect(MockMvcResultMatchers.model().attributeExists("articles"));
     }
@@ -36,12 +34,11 @@ class ArticleControllerTest {
         // when & then
         mockMvc.perform(MockMvcRequestBuilders.get("/articles/1"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.TEXT_HTML))
+                .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(MockMvcResultMatchers.view().name("articles/detail"))
                 .andExpect(MockMvcResultMatchers.model().attributeExists("article"))
                 .andExpect(MockMvcResultMatchers.model().attributeExists("articleComments"));
     }
-
 
     @DisplayName("[view] [GET] 게시글 검색 페이지 - 정상 호출")
     @Test
@@ -50,9 +47,8 @@ class ArticleControllerTest {
         mockMvc.perform(MockMvcRequestBuilders.get("/articles/search"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.view().name("articles/search"))
-                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.TEXT_HTML));
+                .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
     }
-
 
     @DisplayName("[view] [GET] 게시글 해시태그 검색 페이지 - 정상 호출")
     @Test
@@ -61,7 +57,7 @@ class ArticleControllerTest {
         mockMvc.perform(MockMvcRequestBuilders.get("/articles/search-hashtag"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.view().name("articles/search-hashtag"))
-                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.TEXT_HTML));
+                .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
     }
 
 
